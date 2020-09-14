c4709e18e0f126a771d3d59ae5f4b0d620906019
VitaliiShyliuk <71257918+VitaliiShyliuk@users.noreply.github.com>
2020-09-14 12:49:42 +0300

    Update Task19.java

--- Integer/Task19.java
+++ Integer/Task19.java
@@ -5,9 +5,9 @@ package ua.univer.base.lesson01.HW01.Integer;
 public class Task19 {
     public static void main(String[] args) {
         Task19 t = new Task19();
-        t.outPrint(86400);
+        t.countMinutes(86400);
     }
-    void outPrint(int a) {
+    void countMinutes(int a) {
         if (a>0 | a <86401) {
             a = (a - (a % 60)) / 60;
             System.out.println("Количество полных минут, прошедших с начала суток:");
